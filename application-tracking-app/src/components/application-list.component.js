508d8ffb85c1caec5766e9758dd55ce7ce6a5572
Kinjalben Dhimmar <kinjaldhimmar98@gmail.com>
2021-03-13 12:11:21 -0500

    Implemented UI for application list and completed create user

--- application-tracking-app/src/components/application-list.component.js
+++ application-tracking-app/src/components/application-list.component.js
@@ -1,11 +1,61 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
+const Application = props =>(
+    <tr>
+        <td>{props.application.username}</td>
+        <td>{props.application.company}</td>
+        <td>{props.application.link}</td>
+        <td>{props.application.position}</td>
+        <td>{props.application.data}</td>
+    </tr>
+)
 
 export default class ApplicationList extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {applications : []};
+        
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/applications/')
+            .then(response => 
+            {
+                this.setState({applications : response.data})
+            })
+            .catch((error)=> {
+                console.log(error);
+            })
+    }
+
+    applicationList(){
+        return this.state.applications.map(currentapplication => {
+            return <Application application={currentapplication}/>;
+        })
+    }
+
     render(){
         return(
             <div>
-                <p>Application List Component</p>
+                <h3>Applications</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Company</th>
+                            <th>Link</th>
+                            <th>Position</th>
+                            <th>Date</th>   
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.applicationList()}
+                    </tbody>
+                </table>
             </div>
-        )
+        );
     }
 }
--- application-tracking-app/src/components/create-application.component.js
+++ application-tracking-app/src/components/create-application.component.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
 
 export default class CreateApplication extends Component{
 
@@ -25,9 +26,15 @@ export default class CreateApplication extends Component{
     }
 
     componentDidMount(){
-        this.setState({
-            users: ['test user'],
-            username: 'test user'  
+        axios.get('http://localhost:5000/users/')
+        .then(response => {
+            if (response.data.length > 0)
+            {
+                this.setState({
+                    users: response.data.map(user => user.username),
+                    username: response.data[0].username  
+                })
+            }
         })
     }
 
@@ -74,7 +81,10 @@ export default class CreateApplication extends Component{
 
         console.log(application);
 
-        //window.location = '/';
+        axios.post('http://localhost:5000/applications/add',application)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
     }
 
     render(){
